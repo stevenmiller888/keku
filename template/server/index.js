b4f6d245a77417714f7c68dccaa5c70e4e662b6e
stevenmiller888 <stevenmiller888@me.com>
2015-05-11 21:21:42 -0700

    updated template/server/index.js

--- template/server/index.js
+++ template/server/index.js
@@ -3,16 +3,19 @@
  * Module dependencies.
  */
 
+var stream = require('fs').createReadStream;
 var bodyParser = require('koa-body-parser');
 var logger = require('koa-logger');
-var render = require('./render');
+var join = require('path').join;
+var fs = require('co-fs');
 var koa = require('koa');
 
 /**
- * App.
+ * Configure the app.
  */
 
 var app = koa();
+app.port = process.env.PORT || 3000;
 
 /**
  * Logging middleware.
@@ -27,15 +30,45 @@ app.use(logger());
 app.use(bodyParser());
 
 /**
- * Render.
+ * Serve build.js / build.css
  */
 
-app.use(function *(){
-  yield render('index');
+app.use(function*(next){
+  var skip = !/^\/build\.(?:js|css)$/.test(this.path);
+
+  if (skip || 'GET' != this.method) {
+    yield next;
+    return;
+  }
+
+  var i = this.path.indexOf('.');
+  this.type = this.path.slice(i);
+  this.body = stream(root(this.path.slice(1)));
+});
+
+/**
+ * Serve index.html
+ */
+
+app.use(function*(){
+  if (this.accepts('html')) {
+    this.type = 'html';
+    this.body = stream(root('index.html'));
+  }
 });
 
 /**
  * Listen.
  */
 
-app.listen(process.env.PORT || 3000);
+app.listen(app.port, function(){
+  console.log('\nListening at http://localhost:%s\n', app.port);
+});
+
+/**
+ * Return the root path of `path`.
+ */
+
+function root(path){
+  return join(process.cwd(), path);
+}
